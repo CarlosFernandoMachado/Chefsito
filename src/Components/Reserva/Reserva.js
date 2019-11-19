718a99d77dce9aa057e47bdbc761c4169aea522c
Andrew22674 <avela@unitec.edu>
2019-11-18 23:19:05 -0600

    reservation style changes

--- src/Components/Reserva/Reserva.js
+++ src/Components/Reserva/Reserva.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './Reserva.css';
 import TextField from '@material-ui/core/TextField';
-
-import Grid from '@material-ui/core/Grid';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+//import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+/*import ButtonGroup from '@material-ui/core/ButtonGroup';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Grow from '@material-ui/core/Grow';
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
-import MenuList from '@material-ui/core/MenuList';
+import MenuList from '@material-ui/core/MenuList';*/
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
+//import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import resImage from './reservar.png';
 
 
@@ -44,89 +50,139 @@ class Reserva extends Component {
   constructor() {
     super();
     this.state = {
-       nombre: null,
-       apellido: null,
-       departamento: null,
-       direccion: null,
-       telefono: null,
-       comboBoxOption:''
+      nombre: null,
+      apellido: null,
+      departamento: null,
+      direccion: null,
+      telefono: null,
+      chef: '',
+      selectedDate: null,
 
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.onClickBtn = this.onClickBtn.bind(this);
-      /*this.handleClick = this.handleClick.bind(this);
-      this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
-      this.handleToggle = this.handleToggle.bind(this);
-      this.handleClose = this.handleClose.bind(this);*/
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onClickBtn = this.onClickBtn.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    /*this.handleClick = this.handleClick.bind(this);
+    this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);*/
 
   }
   handleChange = name => event => {
     this.setState({ [name]: event.target.value });
     console.log(event.target.value);
-}
+  }
+
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+    console.log(date);
+  };
 
   handleSubmit = (event) => {
     //Make a network call somewhere
     event.preventDefault();
   }
 
-  onClickBtn(){
-    console.log(this.state.nombre);
-    console.log(this.state.comboBoxOption);
+  onClickBtn() {
+    console.log(this.state);
+
   }
-/*
-  handleClick = () => {
-    console.info(`You clicked ${options[selectedIndex]}`);
-  };
-
-  handleMenuItemClick = (event, index) => {
-  setSelectedIndex(index);
-    setOpen(false);
-  };
-
-  handleToggle = () => {
-    setOpen(prevOpen => !prevOpen);
-  };
-
-  handleClose = event => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };*/
+  /*
+    handleClick = () => {
+      console.info(`You clicked ${options[selectedIndex]}`);
+    };
+  
+    handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+      setOpen(false);
+    };
+  
+    handleToggle = () => {
+      setOpen(prevOpen => !prevOpen);
+    };
+  
+    handleClose = event => {
+      if (anchorRef.current && anchorRef.current.contains(event.target)) {
+        return;
+      }
+  
+      setOpen(false);
+    };*/
 
   render() {
-    const { classes } = this.props;
 
     return (
-      <div id = "reserve-div">
+      <div id="reserve-div">
 
-        <form id = "reserve-form" onSubmit={this.handleSubmit}>
-        <h1 id="main-title">Reservar</h1>
-          <TextField style = {{width: '75%'}} name="name" label = "Nombre" onChange={this.handleChange('nombre')} />
-          <TextField style = {{width: '75%'}} name="surname" label = "Apellido" onChange={this.handleChange('apellido')} />
-          <TextField style = {{width: '75%'}} name="region" label = "Departamento" onChange={this.handleChange('departamento')} />
-          <TextField style = {{width: '75%'}} name="address" label = "Dirección" onChange={this.handleChange('direccion')} />
-          <TextField style = {{width: '75%'}} name="phone" label = "Número de teléfono" onChange={this.handleChange('telefono')} />
-          <TextField style = {{width: '75%'}} name="opt6" label = "Opción 6" onChange={this.handleChange('telefono')} />
+        <form id="reserve-form" onSubmit={this.handleSubmit}>
+          <h1 id="main-title">Reservar</h1>
+          <div id="inputs">
+            <TextField style={{ width: '75%' }} name="name" label="Nombre" onChange={this.handleChange('nombre')} />
+            <TextField style={{ width: '75%' }} name="surname" label="Apellido" onChange={this.handleChange('apellido')} />
+            <TextField style={{ width: '75%' }} name="region" label="Departamento" onChange={this.handleChange('departamento')} />
+            <TextField style={{ width: '75%' }} name="address" label="Dirección" onChange={this.handleChange('direccion')} />
+            <TextField style={{ width: '75%' }} name="phone" label="Número de teléfono" onChange={this.handleChange('telefono')} />
 
-          <FormControl >
-        <InputLabel id="select-option">Seleccionar</InputLabel>
-        <Select
-          //labelId="select-label"
-          //id="demo-simple-select"
-          value={this.state.comboBoxOption}
-          //onChange={this.handleChange}
-          onChange={this.handleChange('comboBoxOption')}
-        >
-          <MenuItem value={'option 1'}>1</MenuItem>
-          <MenuItem value={'option 2'}>2</MenuItem>
-          <MenuItem value={'option 3'}>3</MenuItem>
-        </Select>
-      </FormControl>
-      {/*
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+
+              <div id = "date-div">
+                <KeyboardDatePicker
+                  margin="normal"
+                  id="date-picker-dialog"
+                  label="Fecha"
+                  format="dd/MM/yyyy"
+                  value={this.state.selectedDate}
+                  onChange={this.handleDateChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+
+                <KeyboardTimePicker
+                  margin="normal"
+                  id="time-picker"
+                  label="Hora"
+                  value={this.state.selectedDate}
+                  onChange={this.handleDateChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                  }}
+                />
+              </div>
+
+
+            </MuiPickersUtilsProvider>
+
+            <FormControl style={{ width: '75%' }}>
+
+            <InputLabel id="select-option">Seleccionar</InputLabel>
+            <Select
+
+              //labelId="select-label"
+              //id="demo-simple-select"
+              value={this.state.chef}
+              //onChange={this.handleChange}
+              onChange={this.handleChange('chef')}
+            >
+              <MenuItem value={'Jane Doe'}>Jane Doe</MenuItem>
+              <MenuItem value={'John Doe'}>John Doe</MenuItem>
+              <MenuItem value={'Juan Pérez'}>Juan Pérez</MenuItem>
+              <MenuItem value={'Maria Pérez'}>Juan Pérez</MenuItem>
+            </Select>
+          </FormControl>
+          <div id = "divider" />
+          <Button id = "button" onClick={this.onClickBtn} name="OK" label="Submit" type="submit" >OK</Button>
+          <div id = "divider" />
+ 
+          </div>
+
+
+
+
+
+          
+          {/*
       <Grid container direction="column" alignItems="center">
       <Grid item xs={12}>
         <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
@@ -174,19 +230,17 @@ class Reserva extends Component {
     </Grid>
       
       */}
-      
-          <Button onClick = {this.onClickBtn} style = {{width: '75%'}} name = "OK" label="Submit" type="submit" >OK</Button>
+
+          
         </form>
-        
+
         <div >
-        <img style={{width:'100%',height:'100%', paddingTop: '40px'}} resizeMode='contain' id="proj-icon" src={resImage}></img>
+          <img style={{ width: '100%', height: '100%', paddingTop: '6%' }} resizeMode='contain' id="optionalstuff" src={resImage}></img>
         </div>
       </div>
     );
   }
 }
 
-Reserva.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+
 export default Reserva;
